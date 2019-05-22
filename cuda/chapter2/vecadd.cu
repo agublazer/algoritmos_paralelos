4b433df63e38b28c7f236f60faf0c19f3eeb8cfc
Mauro <dany.diaz@ucsp.edu.pe>
2019-05-22 15:57:39 -0500

    Create vecadd.cu

---
+++ cuda/chapter2/vecadd.cu
@@ -0,0 +1,66 @@
+#include <cuda.h>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+void vecAdd1(float *h_A, float* h_B, float* h_C, int n) {
+        for (int i = 0; i < n; i++) {
+                h_C[i] = h_A[i] + h_B[i];
+        }
+}
+
+__global__
+void vecAddKernel(float* A, float* B, float* C, int n) {
+        int i = blockDim.x*blockIdx.x + threadIdx.x;
+        if (i < n) {
+                C[i] = A[i] + B[i];
+        }
+}
+
+void vecAdd2(float* A, float* B, float* C, int n) {
+        int size = n * sizeof(float);
+        float *d_A, *d_B, *d_C;
+        cudaMalloc(&d_A, size);
+        cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
+        cudaMalloc(&d_B, size);
+        cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
+
+        cudaMalloc(&d_C, size);
+
+        vecAddKernel <<< ceil(n/256.0), 256 >>> (d_A, d_B, d_C, n);
+        
+        cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
+
+        cudaFree(d_A);
+        cudaFree(d_B);
+        cudaFree(d_C);
+}
+
+int main() {
+        float *h_A, *h_B, *h_C;
+        int n = 10;
+        h_A = (float*)malloc(n * sizeof(float));
+        h_B = (float*)malloc(n * sizeof(float));
+        h_C = (float*)malloc(n * sizeof(float));
+        for (int i = 0; i < n; i++) {
+                h_A[i] = 10.0;
+                h_B[i] = 20.0;
+                h_C[i] = 0.0;
+        }
+
+	      vecAdd2(h_A, h_B, h_C, n);
+
+        for(int i = 0; i<n; i++){
+                printf("%f ", h_C[i]);
+        }
+	      printf("\n")
+
+        free(h_A);
+        free(h_B);
+        free(h_C);
+
+        return 0;
+}
