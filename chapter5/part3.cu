f7c34578d88a584e1cbaf8648f1faed3ffefe869
Mauro <dany.diaz@ucsp.edu.pe>
2019-06-27 23:16:34 -0500

    Update part3.cu

--- chapter5/part3.cu
+++ chapter5/part3.cu
@@ -1,18 +1,17 @@
 // Kernel basado en 5.1 (Ejercicio 3)
 __global__
 void 51basedkernel(float* X, float* Y, int size) {
-	extern __shared__ float partialSum[];
+	extern __shared__ float partialSum[]; 
 	unsigned int t = threadIdx.x;
-	unsigned int i = ;
 	partialSum[t] = 0;
-	if (blockIdx.x*(blockDim.x*2) + threadIdx.x < size) {
+	if (blockIdx.x*(blockDim.x*2) + threadIdx.x < size) { // (1)
 		partialSum[t] = X[blockIdx.x*(blockDim.x*2) + threadIdx.x];
-    partialSum[t] += X[blockIdx.x*(blockDim.x*2) + threadIdx.x + blockDim.x];
+    		partialSum[t] += X[blockIdx.x*(blockDim.x*2) + threadIdx.x + blockDim.x];
 	}
 	__syncthreads();
-	for (unsigned int stride = blockDim.x/2; stride > 0; stride >>= 1) {
+	for (unsigned int stride = blockDim.x/2; stride > 0; stride >>= 1) { // (2)
 		if (t < stride) {
-			partialSum[t] += partialSum[t + stride];
+			partialSum[t] += partialSum[t + stride]; // (3)
 		}
 		__syncthreads();
 	}
